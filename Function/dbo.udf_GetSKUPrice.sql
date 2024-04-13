76e6bfe4814f6b44bc203856fcf86b20a7af81fb
Winston Ambrose <125760502+unleashed-bearman@users.noreply.github.com>
2024-04-13 16:34:12 +0300

    Update dbo.udf_GetSKUPrice.sql

--- Function/dbo.udf_GetSKUPrice.sql
+++ Function/dbo.udf_GetSKUPrice.sql
@@ -4,9 +4,11 @@ create or alter function dbo.udf_GetSKUPrice(
 returns decimal(18, 2)
 as
 begin
-	return cast((select sum(b.Value)/sum(b.Quantity)
-				from dbo.Basket as b 
-				where b.ID_SKU = @ID_SKU) as decimal(18, 2))
+	return cast(
+		(select sum(b.Value)/sum(b.Quantity)
+		from dbo.Basket as b 
+		where b.ID_SKU = @ID_SKU) as decimal(18, 2)
+		)
 end
 
 
