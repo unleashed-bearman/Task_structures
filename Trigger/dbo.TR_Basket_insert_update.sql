4918d67fb42ceef7b156ea7d88a361ff402bd384
Winston Ambrose <125760502+unleashed-bearman@users.noreply.github.com>
2024-04-13 15:10:24 +0300

    Update dbo.TR_Basket_insert_update.sql

--- Trigger/dbo.TR_Basket_insert_update.sql
+++ Trigger/dbo.TR_Basket_insert_update.sql
@@ -5,7 +5,7 @@ begin
 	update dbo.Basket
 	set DiscountValue = [Value] * 0.05
 	where ID_SKU in	(select ID_SKU 
-                  from inserted 
-                  group by ID_SKU
-					        having count(*) >= 2)
+                  	from inserted 
+                  	group by ID_SKU
+			having count(*) >= 2)
 end
